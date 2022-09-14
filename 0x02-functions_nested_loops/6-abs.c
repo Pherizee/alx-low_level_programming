60dc236ebf2f45ef425469f17f82192392134b97
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-09-14 18:37:43 +0100

    Add absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of input
+ * @n: int input
+ * Return: int absolute value
+ */
+int _abs(int n)
+{
+	int absolute;
+
+	absolute = abs(n);
+	return (absolute);
+}
