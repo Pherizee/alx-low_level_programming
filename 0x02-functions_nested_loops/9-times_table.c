553e3d71e56a21c3141f959960f834b027737e41
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-09-14 20:38:43 +0100

    Add times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints up to 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i * j;
+			if ((product / 10) > 0)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					continue;
+				}
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(product + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					continue;
+				}
+				_putchar('\n');
+			}
+		}
+	}
+}
