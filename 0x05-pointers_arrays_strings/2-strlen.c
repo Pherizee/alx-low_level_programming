6e703da18f442412b8c40be5d592dfdbc0b52f72
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-09-25 14:00:02 +0100

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string address
+ *
+ * Return: value of string length
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(s);
+	return (n);
+}
