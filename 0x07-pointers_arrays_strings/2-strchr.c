02c34a3fe7be460e1ed93f2c1ae4b817ceb8eccc
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-09-27 04:53:18 +0100

    fix strchr 1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - function that locates a character in a string
@@ -18,9 +19,9 @@ char *_strchr(char *s, char c)
 		if (*s == c)
 		{
 			mem = s;
-			break;
+			return (mem);
 		}
 		s++;
 	}
-	return (mem);
+	return (NULL);
 }
