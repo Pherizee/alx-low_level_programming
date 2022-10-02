135adb9eef3731722796f4a5b3445dd290995897
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-02 11:33:41 +0100

    add 3 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int c;
+	unsigned long int fib, i, j;
+
+	i = 1;
+	j = 2;
+
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+
+	for (c = 0; c < 96; c++)
+	{
+		fib = i + j;
+		if (c == 95)
+			printf("%lu\n", fib);
+		else
+			printf("%lu, ", fib);
+		i = j;
+		j = fib;
+	}
+
+	return (0);
+}
