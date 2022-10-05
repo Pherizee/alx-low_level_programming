e2944844802e9a0137879b889ff3cdce0d3b22f5
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-05 05:26:05 +0100

    please alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,12 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			free(array);
+			for (j = 0; j <= i; j++)
+				free(array[j]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
