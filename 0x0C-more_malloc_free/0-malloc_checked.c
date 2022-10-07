4fd10c4729ad9ebb61b3936fd896e31abc11f7fe
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-07 04:51:19 +0100

    add malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memmory using malloc
+ *
+ * @b: size of memory
+ *
+ * Return: void pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
