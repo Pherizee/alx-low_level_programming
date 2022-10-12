5a169bf50e56a74897cff5885693a15722bf0887
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-12 04:27:49 +0100

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: Success 0 always
+ *
+ * description: print name of present file
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
