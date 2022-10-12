98403c54788c71f83d66d93d6b32713d50108cbe
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-12 05:04:33 +0100

    add dog.h 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog {
+/**
+ * struct dog - structure for dog information
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
