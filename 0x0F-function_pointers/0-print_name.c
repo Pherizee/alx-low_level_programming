2616b1237101c4bad1b7395bbf270a02e70fb305
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-13 01:37:43 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints names
+ *
+ * @name: name to be printed
+ * *f: function pointer
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
