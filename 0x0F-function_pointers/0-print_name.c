3f981c132ef886268409c5056a48a2ffdb41f3ff
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-13 01:40:15 +0100

    print name 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,15 @@
  * print_name - prints names
  *
  * @name: name to be printed
- * *f: function pointer
+ * @f: function pointer
  *
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
