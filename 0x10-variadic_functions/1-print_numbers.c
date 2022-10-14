eddeb5f0498cdc96c5c33308f3f9d42163327309
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-10-14 04:25:23 +0100

    print numbers 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
-		printf("%d", x);;
+		printf("%d", x);
 		if (i + 1 < n && separator != NULL)
 			printf("%s", separator);
 	}
