2d112dd67d449580757384fb89633e3e7114a718
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2023-07-04 00:27:11 +0100

    fix print number -1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-char *_itoa(int n, char *numStr);
 /**
  * print_number - print number
  *
@@ -9,60 +8,25 @@ char *_itoa(int n, char *numStr);
 
 void print_number(int n)
 {
-	char numStr[12];
-
-	char *str = _itoa(n, numStr);
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
- * _itoa - convert integer to string
- * @n: number to be converted
- * @numStr: variable to hold the number string
- * Return: string representation of number
-*/
-
-char *_itoa(int n, char *numStr)
-{
-	int dup = n, length = 0, index, digit;
+	unsigned int i;
 
 	/* if n is zero */
-	if (dup == 0)
+	if (n == 0)
 	{
-		numStr[0] = '0';
-		numStr[1] = '\0';
-		return (numStr);
-	}
-	/* get the length of the num str */
-	while (dup != 0)
-	{
-		length++;
-		dup /= 10;
+		_putchar('0');
+		return;
 	}
 	/* if n is negative */
 	if (n < 0)
 	{
+		_putchar('-');
 		n = -n;
-		length++;
-		numStr[0] = '-';
 	}
 
-	index = length - 1;
-	while (n != 0)
-	{
-		digit = n % 10;
-		numStr[index] = digit + '0';
-		n /= 10;
-		index--;
-	}
-	/* add the string termination */
-	numStr[length] = '\0';
+	i = n;
 
-	return (numStr);
+	if (i / 10 != 0)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
 }
