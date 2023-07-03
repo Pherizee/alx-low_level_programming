4141c52548242aa789c80a14d2e4cbb3b6a03003
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2023-07-03 22:14:12 +0100

    fix leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,15 @@ char *leet(char *str)
 {
 	int i = 0, j = 0;
 	char alpha[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char code[5] = {'4', '3', '0', '7', '1'};
+	char code[10] = {'4', '4', '3', '3', '0', '0', '7, '7', '1', '1'};
 
 	while (str[i])
 	{
 		for (; j < 10; i++)
+		{
 			if (str[i] == alpha[j])
-				str[i] = code[j / 2];
+				str[i] = code[j];
+		}
 
 		i++;
 	}
